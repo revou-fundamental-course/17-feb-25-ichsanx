798b90bac02f4b456cd369dc2c3861fa23cc6e55
Ichsan Saputra <ichsan.saputra007@gmail.com>
2025-02-27 04:08:04 +0700

    Update jss

--- js/script.js
+++ js/script.js
@@ -1,43 +1,15 @@
 // Ini Javascript External
 
-function validateForm() {
-    let nameInput = document.getElementById('name-input');
+let banners = document.querySelectorAll('.main-banner');
+let index = 0;
 
-    // Kondisi Validasinya
-    if (nameInput.value == '') {
-        alert('Nama tidak boleh kosong');
-    } else {
-        alert('Welcome ' + nameInput.value);
-    }
+function showNextBanner() {
+    banners.forEach((banner, i) => {
+        banner.style.display = i === index ? "block" : "none";
+    });
+
+    index = (index + 1) % banners.length;
 }
 
-// For Index Identifier
-let indexBanner = 0;
-
-showBanner();
-
-function nextBanner() {
-    // Increment IndexBanner
-    indexBanner += 1;
-    showBanner();
-}
-
-function showBanner() {
-    let banner = document.getElementsByClassName('main-banner');
-
-    // Reset Condition
-    if (indexBanner > banner.length - 1) {
-        indexBanner = 0;
-    }
-
-    // Loop untuk hide semua
-    for (let i = 0; i < banner.length; i++) {
-        banner[i].style.display = 'none';
-    }
-
-    // Show Banner
-    banner[indexBanner].style.display = 'block';
-}
-
-// Auto Play
-setInterval(nextBanner, 3000);
+setInterval(showNextBanner, 3000);
+showNextBanner();
